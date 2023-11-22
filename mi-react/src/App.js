7f067d108da851575957e52e907362545df6ab7c
Celeste <139517444+Cele1202@users.noreply.github.com>
2023-11-22 19:27:06 -0300

    el mensaje

--- mi-react/src/App.js
+++ mi-react/src/App.js
@@ -14,16 +14,7 @@ function App() {
       testimonio: 'Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software.',
       final: 'FreeCodeCamp cambió mi vida.'
     },
-    {
-      imagen: 'sara',
-      nombre: 'Sarah Chima',
-      pais: 'Nigeria',
-      cargo: 'Ingeniera de Software',
-      empresa: 'ChatDesk',
-      inicio: 'FreeCodeCamp fue la puerta de entrada a mi carrera',
-      testimonio: ' como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'
-    }
-   
+    
   ];
 
   return (
